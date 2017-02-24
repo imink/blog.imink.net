eb949ebc610b07b96f867d9fc7189de3631f554c
Patrick <patrick.wang@gmail.com>
2017-02-24 19:58:51 +0800

    add config

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+'use strict';
 var fs = require('fs');
-
 var marked = require('marked');
-
+var yaml = require('js-yaml');
+var helper = require('./app/utilities/utilities');
 
 
 var TemplateEg = function(tplt, data) {
 	var re = /<%([^%>]+)?%>/g;
+  var match;
   while(match = re.exec(tplt)) {
-  	tplt = tplt.replace(match[0], data);
+  	tplt = tplt.replace(match[0], data[match[1]]);
   }
   return tplt;
 };
@@ -17,7 +19,9 @@ var tplt = fs.readFile('./app/template/index.html', 'utf-8', function(err, data)
 	var tplt = data;
 	var sample = fs.readFile('./app/archive/sample.md', 'utf-8', function(err, data) {
 		var content = marked(data);
-		var output = TemplateEg(tplt, content);	
+    var post = helper.getPostMeta('./app/archive/sample.yml');
+    post.content = content;
+		var output = TemplateEg(tplt, post);	
 		// console.log(output);
 
 		fs.writeFile('./index.html', output, (err) => {
@@ -27,32 +31,10 @@ var tplt = fs.readFile('./app/template/index.html', 'utf-8', function(err, data)
 	});
 });
 
-// copy the css js files 
-var copyStaticFiles = function(src, dst, cb) {
-	var cbCalled = false;
 
-  var rd = fs.createReadStream(src);
-  rd.on("error", function(err) {
-    done(err);
-  });
-  var wr = fs.createWriteStream(dst);
-  wr.on("error", function(err) {
-    done(err);
-  });
-  wr.on("close", function(ex) {
-    done();
-  });
-  rd.pipe(wr);
 
-  function done(err) {
-    if (!cbCalled) {
-      cb(err);
-      cbCalled = true;
-    }
-  }
-};
 
-copyStaticFiles('./app/template/css/custom.css', './public/css/custom.css', function(err) {
+helper.copyStaticFiles('./app/template/css/custom.css', './public/css/custom.css', function(err) {
 	if (err) throw err;
 });
 
